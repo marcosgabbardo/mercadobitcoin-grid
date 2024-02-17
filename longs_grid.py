4d40e0545aba15214df934bdf9566c35c859a7e4
Marcos Gabbardo <2336022+marcosgabbardo@users.noreply.github.com>
2024-02-17 18:11:42 -0300

    Update longs_grid.py

--- longs_grid.py
+++ longs_grid.py
@@ -2,14 +2,13 @@ import utils
 import mercadobitcoin
 from mercadobitcoin import TradeApi
 from datetime import datetime
-from datetime import timedelta
+# from datetime import timedelta
 import time
 
-
-split = 6  # split the money in n orders
-spread = 1.50  # 1.50% below compared with ticker.last_price
-sleep = 900  # sleep n seconds
-min_balance = 500  # minimum balance to start buy orders
+split = 2  # split the money in n orders
+spread = 0.5  # 1.50% below compared with ticker.last_price
+sleep = 180  # sleep n seconds
+min_balance = 100  # minimum balance to start buy orders
 
 mbtcapi = mercadobitcoin.Api()
 mbtctradeapi = TradeApi(b'INSERT YOUR CLIENT ID HERE',
@@ -18,7 +17,7 @@ mbtctradeapi = TradeApi(b'INSERT YOUR CLIENT ID HERE',
 while True:
     orders = utils.list_open_orders(mbtctradeapi)
 
-    if orders.empty == True:
+    if orders.empty:
         df_ticker = utils.ticker(mbtcapi)
         df_balance = utils.get_account_info(mbtctradeapi)
 
@@ -42,16 +41,19 @@ while True:
             print('')
 
             time.sleep(sleep)
+        else:
+            exit()
 
     else:
 
         now = datetime.utcnow()
-        if orders['created_timestamp'].iloc[0] + timedelta(seconds=sleep) < now:
+        # if orders['created_timestamp'].iloc[0] + timedelta(seconds=sleep) < now:
 
-            date_time = now.strftime("%m/%d/%Y %H:%M:%S")
-            print('############ - ', date_time, ' - ############')
+        date_time = now.strftime("%m/%d/%Y %H:%M:%S")
+        print('############ - ', date_time, ' - ############')
 
-            for index, row in orders.iterrows():
-                mbtctradeapi.cancel_order(coin_pair="BRLBTC", order_id=row['order_id'])
-                print('order_id: ', str(row['order_id']), ' - CANCELED')
-            print('')
+        for index, row in orders.iterrows():
+            mbtctradeapi.cancel_order(coin_pair="BRLBTC", order_id=row['order_id'])
+            print('order_id: ', str(row['order_id']), ' - CANCELED')
+        print('')
+
